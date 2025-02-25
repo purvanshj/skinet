a4490a0017995e2cd29a8a44c794c3a24d176e7a
Purvansh Jain <purvanshj946@gmail.com>
2025-02-26 01:43:43 +0530

    Sort,Search,Filter,Pagination functionality addition

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -21,6 +21,9 @@ public class SpecificationEvaluator<T> where T : BaseEntity
         if(spec.IsDistinct){
             query = query.Distinct();
         }
+        if(spec.IsPagingEnabled){
+            query = query.Skip(spec.Skip).Take(spec.Take);
+        }
         return query;
     }
 
@@ -44,6 +47,9 @@ public class SpecificationEvaluator<T> where T : BaseEntity
 
         if(spec.IsDistinct){
             selectQuery = selectQuery?.Distinct();
+        }
+        if(spec.IsPagingEnabled){
+            selectQuery = selectQuery?.Skip(spec.Skip).Take(spec.Take);
         }
         return selectQuery ?? query.Cast<TResult>();
     }
